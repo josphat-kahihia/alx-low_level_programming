8e090d48736096d065a7eb8fe5829c25d1be6f0e
Josphat Kahihia <josphatkahihia@gmail.com>
2023-10-05 08:00:51 -0700

    Add 100-intel, 4-puts.c, 5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - output string using puts
+ * @s :String for message
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	char s[] = "\"Programming is like building a multilingual puzzle";
+	puts(s);
+
+	return(0);	
+}
