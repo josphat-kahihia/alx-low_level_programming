4f33ce909efccb7b98dcdc5c3bc48a2459b494e9
josphat-kahihia <106393878+josphat-kahihia@users.noreply.github.com>
2023-10-07 03:26:44 +0300

    Update 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase and uppercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
