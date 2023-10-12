bff6223d831d0df7d9e0cf3a008a6eb849c1ea11
josphat-kahihia <106393878+josphat-kahihia@users.noreply.github.com>
2023-10-13 01:41:02 +0300

    Update 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two ints
+ *
+ * Return: result of a multiplied by b
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
