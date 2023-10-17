ede34c3b573a92274b67420524c336792251cfc3
josphat-kahihia <106393878+josphat-kahihia@users.noreply.github.com>
2023-10-17 05:06:14 +0300

    Update 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Finds the lenght of char array
+ * @s: The array character to be checked when the function is called
+ *
+ * Return: Integer
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+}
