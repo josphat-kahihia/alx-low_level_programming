07c52743b51385193ad23a57ab0c63622178f7bf
josphat-kahihia <106393878+josphat-kahihia@users.noreply.github.com>
2023-10-24 05:43:01 +0300

    Update 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints integer
+ * @n: integer to print
+ *
+ * Return: Always (0)
+ */
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+
+	{
+		print_number(n  / 10);
+	}
+
+	_putchar('0' + (n % 10));
+}
