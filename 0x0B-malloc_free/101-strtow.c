41d736cb426a098b7b62252972e4c580316c3421
josphat-kahihia <106393878+josphat-kahihia@users.noreply.github.com>
2023-11-01 01:50:08 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+/**
+ * strtow - Split a string into words.
+ *
+ * @str: The string to be split.
+ *
+ * Returns: An array of strings, each containing a single word. The last
+ * element of the array is NULL.
+ */
 char **strtow(char *str)
 {
   /* Check if the input string is null or empty. */
@@ -29,10 +39,14 @@ char **strtow(char *str)
   char *start = str;
   for (char *p = str; *p != '\0'; p++) {
     if (*p == ' ') {
-      /* Create a new string from the start of the word to the current position. */
+      /* Create a new string from the start of the word to the current
+       * position. */
       char *word = malloc(sizeof(char) * (p - start + 1));
       if (word == NULL) {
         /* Free the memory allocated for the array of strings. */
+        for (int i = 0; i < word_index; i++) {
+          free(words[i]);
+        }
         free(words);
         return NULL;
       }
@@ -69,4 +83,33 @@ char **strtow(char *str)
   words[word_index] = last_word;
 
   return words;
+}
+
+/*
+ * main - Test the strtow function.
+ *
+ * Returns: 0 on success, 1 on failure.
+ */
+int main() {
+  char *str = "This is a test string.";
+
+  char **words = strtow(str);
+
+  if (words == NULL) {
+    printf("Error splitting string into words.\n");
+    return 1;
+  }
+
+  /* Print the words. */
+  for (int i = 0; words[i] != NULL; i++) {
+    printf("%s\n", words[i]);
+  }
+
+  /* Free the memory allocated for the array of strings. */
+  for (int i = 0; words[i] != NULL; i++) {
+    free(words[i]);
+  }
+  free(words);
+
+  return 0;
 }
