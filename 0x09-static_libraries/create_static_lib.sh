bf6c56d88cd5b353ffc9da76d470e504c0bd9a57
josphat-kahihia <106393878+josphat-kahihia@users.noreply.github.com>
2023-11-01 00:58:49 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,3 @@
-#! /bin/bash
-
-gcc 0-isupper.c 0-strcat.c 1-isdigit.c 1-strncat.c 2-strlen.c 3-islower.c 3-strcmp.c 4-isalpha.c 5-strstr.c 9-strcpy.c _putchar.c0-memset.c 100-atoi.c 1-memcpy.c 2-strchr.c 2-strncpy.c 3-puts.c 3-strspn.c 4-strpbrk.c 6-abs.c -c
-ar -rc liball.a *.o
-ranlib liball.a
+#!/bin/bash
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
+ar -rcs liball.a *.o
