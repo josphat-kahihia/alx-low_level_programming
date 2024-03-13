f39be9f4b5a598dd53954e24a3cd8a0b580bf3a7
Josphat Kahihia <josphatkahihia@gmail.com>
2024-03-13 03:55:34 +0300

    Update 0-isupper to use Betty Doc and Code style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
  * _isupper - checks for uppercase character
+ * @c: The character's ASCII value passed by the main function
  *
  * Return: 1 if uppercase, 0 if lowercase
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 65) && (c <= 90))
 		return (1);
 	else
 		return (0);
