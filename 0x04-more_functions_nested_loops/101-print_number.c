a55c708f61bb27af2727ddfe72a27bf2858d809d
Josphat Kahihia <josphatkahihia@gmail.com>
2024-03-13 05:02:23 +0300

    Update 101-print_number.c to use Betty Doc style

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,22 @@
 
 /**
  * print_number - prints an integer
- * @n: The integer printed
+ * @n: The integer value passed on to be printed using _putchar
  *
- * Return: void
+ * Description: The function recursively checks whether all digits have been
+ * printed, effectively making sure that it prints from the smallest (one's)
+ * to the very-left end of the number.
+ * Note(1) - The n is not modified in either the recursive function call or
+ * the printing statement, as that would lead to incorrect results.
+ * Note(2) - The use of modulus in the statement acts as a guard, to prevent
+ * the printing of a char that isn't a number.
+ *
+ * Example: Given 123456 as n, the function while make recursive calls
+ * starting with print_number 12345, then 1234, then 123, then 12, then 1,
+ * and as each recursive call starts to get satisfied - starting with the first
+ * one (1), it will print the next number, (2), and so on, until it returns to
+ * the initial one's value (6). Now that each digit has been printed in
+ * succession, we will have 123456.
  */
 void print_number(int n)
 {
