0684f1ba24d01ac15dfdae20eec4e784ba3f49cd
Josphat Kahihia <josphatkahihia@gmail.com>
2024-03-13 04:16:51 +0300

    Update 9-fizzbuzz.c
    The previous version used excess if statements, polluting the code.
    That has now been reduced to four if statements, with the printing
    of spaces being left on it's own check to make sure there's no trailing spaces
    in the output.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,43 +3,25 @@
 /**
  * main - fizzbuzz
  *
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n = 1;
-	int m3, m5;
 
 	while (n <= 100)
 	{
-		m3 = n % 3;
-		m5 = n % 5;
-		if ((m3 == 0) && (m5 == 0))
-		{
-			printf("FizzBuzz ");
-			n++;
-		}
-		else if (m3 == 0)
-		{
-			printf("Fizz ");
-			n++;
-		}
-		else if (m5 == 0)
-		{
-			printf("Buzz ");
-			n++;
-		}
-		else if ((n == 100) && (m5 == 0))
-		{
+		if (n % 15 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
 			printf("Buzz");
-			n++;
-		}
 		else
-		{
-			printf("%d ", n);
-			n++;
-		}
+			printf("%d", n);
+		if (n != 100)
+			printf(" ");
+		n++;
 	}
 	printf("\n");
 
