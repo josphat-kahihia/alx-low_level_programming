50acca7c991a9c0a9bb930da9e55048dafabb3bb
Josphat Kahihia <josphatkahihia@gmail.com>
2024-03-15 15:02:09 +0300

    Rewrite atoi the correct way
    With the newfound understanding of pointers, I decided to rewrite the
    code from ground up, including error checking and verification. Now,
    atoi is a proper atoi. Debug messages have not been removed to be reminders

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,134 @@
 #include "main.h"
+#include <stdio.h>
+#include <limits.h>
 
 /**
- * _atoi -  converts a string to an integer
- * @s: The string to be converted
+ * power - calculates num to power exp
+ * @num: The num
+ * @exp: The power of num
  *
- * Return: The integer result after conversion
+ * Return: The powered number
+ */
+int power(int num, int exp)
+{
+	int i;
+	int result;
+
+	result = 1;
+	for (i = exp; i > 0; i--)
+		result *= num;
+
+	return (result);
+}
+
+/**
+ * numsize - gets the size of the number
+ * @n: The index of the start of the number
+ *
+ * Return: size
+ */
+int numsize(char *n)
+{
+	int size;
+	int i;
+
+	i = 0;
+	while ((*(n + i) >= 48) && (*(n + i) <= 57))
+	{
+		size++;
+		i++;
+	}
+	printf("The size of the number is: 10 ^ %d\n", size);
+	return (size);
+}
+
+/**
+ * getnum - creates the number
+ * @n: The reference index of the start of the number
+ * @numsize: Stores the size of the number
+ *
+ * Return: num if success, -1 if fail or -5 if number > INT_MAX
+ */
+int getnum(char *n)
+{
+	int i;
+	int j;
+	int num;
+	int size;
+
+	i = 0;
+	num = 0;
+	if ((*(n + i) >= 48) && (*(n + i) <= 57))
+	{
+		size = numsize((n + i));
+		if (size > 10)
+		{
+			printf("Error: Size beyond INT_MAX\n");
+			return (-5);
+		}
+		for (j = (size - 1); j >= 0 ; j--)
+		{
+			num += ((*(n + i) - '0') * power(10, j));
+			if (num > INT_MAX)
+			{
+				printf("Error: Number too big for int\n");
+				return (-5);
+			}
+			i++;
+		}
+		printf("Function getnum() output: %d\n", num);
+		return (num);
+	}
+	else
+	{
+		printf("The index is not a number!\n");
+		return (-1);
+	}
+}
+
+/**
+ * _atoi - Returns the integer value
+ * @s: The string we're parsing to get our int
+ *
+ * Description: The function checks the eventual sign, then on finding an ASCII
+ * number, calls getnum to get the number, then returns the number with it's
+ * respective sign
+ *
+ * Return: number if success, -1 if fail, -2 if helper functions fail
  */
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int ism = 0;
+	char *s_main;
+	int i;
+	int num;
+	int pos;
+	int neg;
 
-	while (s[c])
+	s_main = s;
+	i = 0;
+	pos = 0;
+	neg = 0;
+	while (*(s_main + i))
 	{
-		if (s[c] == 45)
+		if (*(s_main + i) == 43) /* Positive sign */
+			pos++;
+		if (*(s_main + i) == 45) /* Negative sign */
+			neg++;
+		if ((*(s_main + i) >= 48) && (*(s_main + i) <= 57))
 		{
-			min *= -1;
+			num = getnum((s_main + i));
+			if (num < 0)
+			{
+				printf("Error: getnum() failed CODE: %d\n", num);
+				return (-2);
+			}
+			if ((pos - neg) < 0)
+				num *= -1;
+			return num;
 		}
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			ism = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-		if (ism == 1)
-		{
-			break;
-		}
-		c++;
+		i++;
 	}
-	ni *= min;
-	return (ni);
+	if ((*(s_main + i)) == '\0')
+		printf("Error, no numbers found!\n");
+	return (-1);
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,9 +9,10 @@
  */
 int _strlen(char *s)
 {
-	int len;
+	int i;
 
-	len = strlen(s);
+	while (*(s + i))
+		i++;
 
-	return (len);
+	return (i);
 }
